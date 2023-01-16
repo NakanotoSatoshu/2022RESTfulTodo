b0d12ac97621d85fbf423ab30371d8e4d6097e75
nakamotosatoshu <okamototarou013@gmail.com>
2023-01-16 11:07:00 +0900

    GoToVue-test

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,19 +1,82 @@
-//jsconfig.json
+const { createApp, ref, onMounted } = Vue;
+const app = Vue.createApp({
+    setup() {
+        const users = ref([]);
+        const name = ref('');
+        //const test = ref({ user_id2: '',item_name2:'',});
 
-// dayjsのインポート
-//import dayjs from './dayjs'
-// ロケールのインポート
-//import './dayjs/locale/ja'
+        const createNewUser = () => {
+          axios
+            .post('https://jsonplaceholder.typicode.com/users', {
+              name: name.value,
+            })
+            .then((response) => users.value.unshift(response.data))
+            .catch((error) => console.log(error));
+        };
+        const thisistest = () => {
+            axios
+              .post('/...', {
+                id: this.id,
+                item_name: this.item_name,
+              })
+              .then((response) => users.value.unshift(response.data))
+              .catch((error) => console.log(error));
+          };
+        const updateOne = () => {
+          console.log("test");
+        };
+        onMounted(() => {
+          axios
+            .get('https://jsonplaceholder.typicode.com/users')
+            .then((response) => (users.value = response.data))
+            .catch((error) => console.log(error));
+    });
+    return {
+      users,
+      name,
+      createNewUser,
+    };
+    },
+    data() {
+      return {
+        todo ,
+        user_id2: '',
+        item_name2:'',
+        user_id: '',
+        item_name:'',
+      }
+    },
+    methods: {
+         updateOne:function(){
+         console.log("GO!!!")
+         const fData = new FormData();
+         //let data = JSON.stringify(user_id2)
+         console.log(this.user_id)
+         //console.log(test.user_id2)
+         //console.log(data)
+         console.log(this.item_name)
+         //console.log(test.item_name2)
+         fData.append( 'user_id2' ,this.user_id);
+         fData.append( 'item_name2' ,this.item_name);
 
-// ロケール設定
-//dayjs.locale('ja')
-const now = new Date()
-const year = now.getFullYear()
-const month = now.getMonth() + 1
-const day = now.getDate()
+         axios
+         .post('/edit', fData, { headers: {'Content-Type':'application/json'} }  )
+         .then((response) =>
+                      (response.data))
+                     // console.log(response.status)
+                     // console.log(response.headers)
+                     // console.log(response.statusText)
+                     // console.log(response.config)
+         .catch((error) =>
+              console.log(error));
+             // console.log(JSON.stringify(error));
+            //  console.log(error.name);
+             // console.log(error.message);
+             // console.log(error.request);
+            //  console.log(error.response);
+
+         }
+    }
+})
 
 
-////var now2 = new Date()
-//var now2 = year + month  + day
-//console.log('うおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお' + now)//
-//createApp(App).mount('#app')
