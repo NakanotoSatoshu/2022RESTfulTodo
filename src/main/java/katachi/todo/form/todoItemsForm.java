7aed34ba5777aeab528f619e701bb88a8f9bc867
nakamotosatoshu <okamototarou013@gmail.com>
2023-03-21 09:42:17 +0900

    とりあえず目下のコントローラーをREST化した

--- src/main/java/katachi/todo/form/todoItemsForm.java
+++ src/main/java/katachi/todo/form/todoItemsForm.java
@@ -17,31 +17,26 @@ import lombok.Data;
 public class todoItemsForm {
 
 	private int id ;
-
-
+	
 	@Range(min = 1 , groups=ValidGroup1.class)
 	@NotNull(groups=ValidGroup2.class)
 	//@NotBlank(groups=ValidGroup1.class)
 	private int user_id;
-
+	
 	@NotBlank(groups=ValidGroup1.class)
 	@Length(min = 1, max = 100, groups=ValidGroup2.class)
 	private String item_name;
-
+	
 	@NotNull(groups=ValidGroup1.class)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date expire_date ;
-
+	
 	@DateTimeFormat(pattern="yyyy/MM/dd")
 	private Date finished_date;
-
+	
 	private int is_delete;
 	private Date create_date_time;
 	private Date update_date_time;
-
 	private int priority;
-
 	private Boolean finishedCheck;
-
-
 }
